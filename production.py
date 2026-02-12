50330e40cf880ad0896c0d31a95b25cd57b5f975
Dilip150693 <dilipshah696@gmail.com>
2026-02-12 22:39:24 +0530

    The modification is done, Change 1 by Dilip

--- production.py
+++ production.py
@@ -5,3 +5,18 @@ print('Feature4')
 a=10
 b=20
 print('The addition is', a+b)
+
+
+def multiplication_or_addition(num1,num2):
+    product = num1*num2
+    if product <=1000:
+        return product
+    else:
+        return num1+num2
+    
+
+result  = multiplication_or_addition(20,30)
+print('The Result is', result)
+
+result= multiplication_or_addition(40,50)
+print('The result is', result)
